3bf723198d0cc780cfe7dd6be74e1023e0b3a967
Gerardo Vitale <gerardo.vitale@thoughtworks.com>
2022-05-09 18:04:42 +0200

    git installation script added

---
+++ scripts/install-git.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install git
+
+git version
+
+git config --global user.name "gerardovitale"
+git config --global user.email "gerardo.vitale.errico@gmail.com"
