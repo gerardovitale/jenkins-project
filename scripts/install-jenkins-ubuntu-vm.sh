256cf9c2d806d56c306cab75e5b2b73573d13c88
Gerardo Vitale <gerardo.vitale@thoughtworks.com>
2022-05-09 18:04:23 +0200

    docker-compose installation cmds added

--- scripts/install-jenkins-ubuntu-vm.sh
+++ scripts/install-jenkins-ubuntu-vm.sh
@@ -15,6 +15,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# install docker
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl enable docker
@@ -24,15 +25,13 @@ sudo groupadd docker
 sudo usermod -aG docker azureuser
 newgrp docker
 
-# run jenkins
+# permissions to run jenkins
 sudo mkdir -p /var/jenkins_home
 sudo chown -R 1000:1000 /var/jenkins_home/
-docker run --name=jenkins \
-        -p 8080:8080 -p 50000:50000 \
-        -v /var/jenkins_home:/var/jenkins_home \
-        --restart unless-stopped \
-        -d jenkins/jenkins:jdk11
 
+# install docker-compose
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # show endpoint
 echo 'Jenkins installed'
